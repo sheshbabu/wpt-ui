2a6c0765147c4bf275335ab52bfc4567085b9157
Sheshbabu <sheshbabu@redmart.com>
2017-07-17 23:16:39 +0800

    Extracting data from runtest response and also error handling

--- services/wpt/wpt-service.js
+++ services/wpt/wpt-service.js
@@ -6,9 +6,16 @@ async function runTest(config) {
   const params = getWptParams(config);
   try {
     const response = await axios.get(RUN_TEST_URL, { params });
-    logger.info({ res: response });
-  } catch (e) {
-    logger.error({ err: e });
+    logger.info({ response: response.data });
+    if (response.data.statusCode === 200) {
+      const testId = response.data.data.testId;
+      const jsonUrl = response.data.data.jsonUrl;
+      logger.info({ testId, jsonUrl });
+    } else if (response.data.statusCode === 400) {
+      throw new Error("Bad request");
+    }
+  } catch (err) {
+    logger.error({ err });
   }
 }
 
