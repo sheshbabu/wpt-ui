bb088104233c44412fdfba784e29985f8394afc9
Sheshbabu <sheshbabu@redmart.com>
2017-07-21 00:07:58 +0800

    Using the WptRunTestError and minor refactoring

--- services/wpt/wpt-service.js
+++ services/wpt/wpt-service.js
@@ -1,18 +1,20 @@
 const axios = require("axios");
 const logger = require("../../util/logger");
+const WptRunTestError = require("../../errors/WptRunTestError");
 
 async function runTest(config) {
   const RUN_TEST_URL = "http://www.webpagetest.org/runtest.php";
   const params = getWptParams(config);
   try {
     const response = await axios.get(RUN_TEST_URL, { params });
-    logger.info({ response: response.data });
-    if (response.data.statusCode === 200) {
-      const testId = response.data.data.testId;
-      const jsonUrl = response.data.data.jsonUrl;
+    const body = response.body;
+    logger.info({ response: body });
+    if (body.statusCode === 200) {
+      const testId = body.data.testId;
+      const jsonUrl = body.data.jsonUrl;
       logger.info({ testId, jsonUrl });
-    } else if (response.data.statusCode === 400) {
-      throw new Error("Bad request");
+    } else if (body.statusCode === 400) {
+      throw new WptRunTestError(body);
     }
   } catch (err) {
     logger.error({ err });
