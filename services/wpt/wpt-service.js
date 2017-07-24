a6053e333ba115d4aa4f2cd4f0ab765ce0148016
Sheshbabu <sheshbabu@redmart.com>
2017-07-24 21:52:05 +0800

    Propagating errors upwards

--- services/wpt/wpt-service.js
+++ services/wpt/wpt-service.js
@@ -5,19 +5,16 @@ const WptRunTestError = require("../../errors/WptRunTestError");
 async function runTest(config) {
   const RUN_TEST_URL = "http://www.webpagetest.org/runtest.php";
   const params = getWptParams(config);
-  try {
-    const response = await axios.get(RUN_TEST_URL, { params });
-    const body = response.body;
-    logger.info({ response: body });
-    if (body.statusCode === 200) {
-      const testId = body.data.testId;
-      const jsonUrl = body.data.jsonUrl;
-      logger.info({ testId, jsonUrl });
-    } else if (body.statusCode === 400) {
-      throw new WptRunTestError(body);
-    }
-  } catch (err) {
-    logger.error({ err });
+  const response = await axios.get(RUN_TEST_URL, { params });
+  const body = response.body;
+  logger.info({ response: body });
+  if (body.statusCode === 200) {
+    const testId = body.data.testId;
+    const jsonUrl = body.data.jsonUrl;
+    logger.info({ testId, jsonUrl });
+    return Promise.resolve({ testId, jsonUrl });
+  } else if (body.statusCode === 400) {
+    throw new WptRunTestError(body);
   }
 }
 
