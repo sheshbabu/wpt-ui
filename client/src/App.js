3fd5b267e97d783976c49101448be2e9a4daf607
Sheshbabu <sheshbabu@redmart.com>
2017-08-06 23:22:37 +0800

    Added theme overrides

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 import NavBar from "./containers/NavBar";
 import MainChart from "./containers/MainChart";
+import { blue600 } from "material-ui/styles/colors";
+
+const muiTheme = getMuiTheme({
+  datePicker: {
+    selectColor: blue600
+  },
+  palette: {
+    pickerHeaderColor: blue600
+  },
+  flatButton: {
+    primaryTextColor: blue600
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <NavBar />
           <MainChart />
