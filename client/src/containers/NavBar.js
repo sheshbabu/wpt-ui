d5e5d2a8cc889d38832e550830f9497d0d6cd756
Sheshbabu <sheshbabu@redmart.com>
2017-08-06 19:06:53 +0800

    Extracted StartNewTestButton to separate file

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,38 +1,18 @@
 import React from "react";
 import AppBar from "material-ui/AppBar";
-import RaisedButton from "material-ui/RaisedButton";
-import PlayArrow from "material-ui/svg-icons/av/play-arrow";
 import { blue600 } from "material-ui/styles/colors";
-
-const color = blue600;
+import StartNewTestButton from "../components/StartNewTestButton";
 
 export default function NavBar() {
   const style = {
-    backgroundColor: color
+    backgroundColor: "white"
   };
   return (
     <AppBar
       title="WebPageTest UI"
+      titleStyle={{ color: blue600 }}
       iconElementRight={<StartNewTestButton />}
       style={style}
-    />
-  );
-}
-
-function StartNewTestButton() {
-  const buttonStyle = {
-    margin: "6px 6px 12px 12px"
-  };
-  const iconStyle = {
-    marginTop: "-2px"
-  };
-  return (
-    <RaisedButton
-      style={buttonStyle}
-      label="Start New Test"
-      labelPosition="before"
-      labelColor={color}
-      icon={<PlayArrow color={color} style={iconStyle} />}
     />
   );
 }
