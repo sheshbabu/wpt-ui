769a868d67a53e3a5a1eb55e3dac6ee5348e3db3
Sheshbabu <sheshbabu@redmart.com>
2017-08-07 22:31:32 +0800

    Sending the correct config to db.init()

--- test/helpers/init-db.js
+++ test/helpers/init-db.js
@@ -2,8 +2,7 @@ const db = require("../../util/db");
 
 function initDb() {
   const config = {
-    client: "pg",
-    connection: process.env.DATABASE_URL
+    dbConnectionString: process.env.DATABASE_URL
   };
   db.init(config);
 }
