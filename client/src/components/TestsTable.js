80c9ef27e13ea7f1b527c12eeedf55f21b56b14b
Sheshbabu <sheshbabu@redmart.com>
2017-08-10 00:40:35 +0800

    Removed mock data and using real data

--- client/src/components/TestsTable.js
+++ client/src/components/TestsTable.js
@@ -9,107 +9,81 @@ import {
   TableRowColumn
 } from "material-ui/Table";
 
+const columns = [
+  "Test Id",
+  "First Paint (FV)",
+  "Start Render (FV)",
+  "Last Visual Change (FV)",
+  "Visual Complete (FV)",
+  "Load Time (FV)",
+  "Fully Loaded (FV)",
+  "Speed Index (FV)",
+  "Requests Made (FV)",
+  "Bytes Downloaded (FV)"
+];
+
 export default function TestsTable(props) {
   return (
     <Paper style={{ width: 900, marginTop: 20, marginBottom: 20 }} zDepth={1}>
       <Table bodyStyle={{ overflow: "visible" }}>
         <TableHeader>
           <TableRow>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Test Id
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              First Paint (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Load Time (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Start Render (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Last Visual Change (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Visual Complete (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Fully Loaded (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Speed Index (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Requests Made (FV)
-            </TableHeaderColumn>
-            <TableHeaderColumn style={{ width: 100 }}>
-              Bytes Downloaded (FV)
-            </TableHeaderColumn>
+            {getColumnHeaders()}
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableRowColumn style={{ width: 100 }}>1xx</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn style={{ width: 100 }}>2xx</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn style={{ width: 100 }}>3xx</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1400 ms</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn style={{ width: 100 }}>4xx</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1350 ms</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn style={{ width: 100 }}>5xx</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-            <TableRowColumn style={{ width: 100 }}>1300 ms</TableRowColumn>
-          </TableRow>
+          {getRows(props.tests)}
         </TableBody>
       </Table>
     </Paper>
   );
+}
+
+function getColumnHeaders() {
+  return columns.map(column => {
+    return (
+      <TableHeaderColumn key={column} style={{ width: 100 }}>
+        {column}
+      </TableHeaderColumn>
+    );
+  });
+}
+
+function getRows(tests) {
+  return tests.map(test => {
+    return (
+      <TableRow key={test.test_id}>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.test_id}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_first_paint}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_start_render}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_last_visual_change}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_visual_complete}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_load_time}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_fully_loaded}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_speed_index}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_requests_made}
+        </TableRowColumn>
+        <TableRowColumn style={{ width: 100 }}>
+          {test.fv_bytes_downloaded}
+        </TableRowColumn>
+      </TableRow>
+    );
+  });
 }
