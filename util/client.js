ded8c29b76fe78a59dcbede3a192a5b12fa443fe
Sheshbabu <sheshbabu@redmart.com>
2017-08-11 23:12:13 +0800

    Doing "npm install" of client deps before running build

--- util/client.js
+++ util/client.js
@@ -3,7 +3,8 @@ const util = require("util");
 const child_process = require("child_process");
 const logger = require("./logger");
 
-const buildPath = path.resolve(process.cwd(), "./client/build");
+const clientPath = path.resolve(__dirname, "../client");
+const buildPath = path.resolve(clientPath, "./build");
 
 async function runBuild() {
   if (process.env.NODE_ENV === "development") {
@@ -12,10 +13,14 @@ async function runBuild() {
 
   try {
     const exec = util.promisify(child_process.exec);
+    const options = { cwd: clientPath };
+
+    logger.info("Installing client dependencies ...");
+    await exec("npm install", options);
+    logger.info("Installing client dependencies done");
+
     logger.info("Building client files ...");
-    await exec("npm run build", {
-      cwd: path.resolve(process.cwd(), "./client")
-    });
+    await exec("npm run build", options);
     logger.info("Building client files done");
   } catch (err) {
     logger.info({ err });
