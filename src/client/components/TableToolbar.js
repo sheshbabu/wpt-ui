1607858baad0482b779f7593a65b9b09d53ffd3c
Sheshbabu <sheshbabu@redmart.com>
2017-08-12 19:42:18 +0800

    Added view details button

--- src/client/components/TableToolbar.js
+++ src/client/components/TableToolbar.js
@@ -3,12 +3,14 @@ import { Toolbar, ToolbarGroup } from "material-ui/Toolbar";
 import Paper from "material-ui/Paper";
 import FlatButton from "material-ui/FlatButton";
 import CompareArrows from "material-ui/svg-icons/action/compare-arrows";
+import InfoOutline from "material-ui/svg-icons/action/info-outline";
 
 export default function TableToolbar(props) {
   return (
     <Paper style={{ width: 900, marginTop: 20 }} zDepth={1}>
       <Toolbar style={{ backgroundColor: "white" }}>
         <ToolbarGroup>
+          <ViewTestDetailsButton {...props} />
           <CompareTestsButton {...props} />
         </ToolbarGroup>
       </Toolbar>
@@ -18,14 +20,29 @@ export default function TableToolbar(props) {
 
 function CompareTestsButton(props) {
   const isDisabled = props.selectedTests.length !== 2;
+
   return (
     <FlatButton
       primary
       disabled={isDisabled}
-      style={{ marginLeft: -15 }}
       label="Compare Tests"
-      labelPosition="before"
+      labelPosition="after"
       icon={<CompareArrows />}
+    />
+  );
+}
+
+function ViewTestDetailsButton(props) {
+  const isDisabled = props.selectedTests.length !== 1;
+
+  return (
+    <FlatButton
+      primary
+      disabled={isDisabled}
+      style={{ marginLeft: -11 }}
+      label="View Details"
+      labelPosition="after"
+      icon={<InfoOutline />}
     />
   );
 }
