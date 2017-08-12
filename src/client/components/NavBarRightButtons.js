11d4b49e38eaafe2aef25d5462efd55e021d6291
Sheshbabu <sheshbabu@redmart.com>
2017-08-12 19:26:08 +0800

    Moved compare button from navbar

--- src/client/components/NavBarRightButtons.js
+++ src/client/components/NavBarRightButtons.js
@@ -1,29 +1,12 @@
 import React from "react";
 import RaisedButton from "material-ui/RaisedButton";
-import FlatButton from "material-ui/FlatButton";
 import PlayArrow from "material-ui/svg-icons/av/play-arrow";
-import CompareArrows from "material-ui/svg-icons/action/compare-arrows";
 
 export default function NavBarRightButtons(props) {
   return (
     <div>
-      <CompareTestsButton />
       <StartNewTestButton onClick={props.onStartNewTestClick} />
     </div>
-  );
-}
-
-function CompareTestsButton() {
-  return (
-    <FlatButton
-      primary
-      style={{
-        margin: "6px 6px 12px 12px"
-      }}
-      label="Compare Tests"
-      labelPosition="before"
-      icon={<CompareArrows />}
-    />
   );
 }
 
