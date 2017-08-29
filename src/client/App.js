d14da4557010db35d1de8c5d4057cea518b1b000
Sheshbabu <sheshbabu@redmart.com>
2017-08-29 22:07:04 +0800

    Redirecting to /tests from / to decouple the home and tests list page

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import getTheme from "./util/get-theme";
 import HomePage from "./containers/HomePage";
 import ComparePage from "./containers/ComparePage";
@@ -11,7 +11,8 @@ export default class App extends React.Component {
       <BrowserRouter>
         <MuiThemeProvider muiTheme={getTheme()}>
           <div>
-            <Route exact path="/" component={HomePage} />
+            <Redirect from="/" to="/tests" />
+            <Route path="/tests" component={HomePage} />
             <Route path="/compare" component={ComparePage} />
           </div>
         </MuiThemeProvider>
